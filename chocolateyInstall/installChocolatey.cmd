5b452a16647a1e8f6d4092826e0ca30eec3c42ed
curtis <curtis@serverascode.com>
2013-02-26 17:02:16 -0700

    Added "-InputFormat none" to the powershell command

--- chocolateyInstall/installChocolatey.cmd
+++ chocolateyInstall/installChocolatey.cmd
@@ -2,4 +2,4 @@
 
 SET DIR=%~dp0%
 
-%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& '%DIR%InstallChocolatey.ps1' %*"
+%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -InputFormat none -NoProfile -ExecutionPolicy unrestricted -Command "& '%DIR%InstallChocolatey.ps1' %*"
